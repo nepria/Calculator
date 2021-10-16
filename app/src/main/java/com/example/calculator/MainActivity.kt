f932fc44b118e493c238227a439d4a5767bf6600
Nepria <discord.akhouri@gmail.com>
2021-10-16 13:21:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,134 @@
+ package com.example.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import org.w3c.dom.Text
+import java.lang.ArithmeticException
+
+class MainActivity : AppCompatActivity() {
+
+    var lastNumric : Boolean = false
+    var lastDot : Boolean = false
+    
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onDigit(view: View) {
+        tvInput.append((view as Button).text)
+        lastNumric = true
+    }
+
+    fun onClear(view: View) {
+        tvInput.text = ""
+        lastNumric = false
+        lastDot =  false
+    }
+    fun onDecimalPoint(view: View) {
+        if(lastNumric && !lastDot) {
+            tvInput.append(".")
+            lastNumric = false
+            lastDot = true
+        }
+    }
+
+    fun onEqual(view: View) {
+        if(lastNumric) {
+            var tvValue = tvInput.text.toString()
+            var prefix = ""
+            try {
+                if(tvValue.startsWith("-")) {
+                    prefix = "-"
+                    tvValue = tvValue.substring(1)
+                }
+
+
+                if(tvValue.contains("-")) {
+
+                    val splitValue = tvValue.split("-")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(!prefix.isEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput.text = removeZeroAfterDot((one.toDouble() - two.toDouble()).toString())
+                }
+
+                else if(tvValue.contains("+")) {
+
+                    val splitValue = tvValue.split("+")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(!prefix.isEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput.text = removeZeroAfterDot((one.toDouble() + two.toDouble()).toString())
+                }
+
+                else if(tvValue.contains("*")) {
+
+                    val splitValue = tvValue.split("*")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(!prefix.isEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput.text = removeZeroAfterDot((one.toDouble() * two.toDouble()).toString())
+                }
+
+                else if(tvValue.contains("/")) {
+
+                    val splitValue = tvValue.split("/")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(!prefix.isEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput.text = removeZeroAfterDot((one.toDouble() /  two.toDouble()).toString())
+                }
+
+
+            }catch (e: ArithmeticException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun removeZeroAfterDot(result : String) : String{
+        var value = result
+        if(result.contains(".0"))
+            value = result.substring(0,result.length-2)
+        return value
+    }
+
+    fun onOperator(view: View) {
+      if(lastNumric && !isOperatorAdded(tvInput.text.toString())) {
+        tvInput.append((view as Button).text)
+          lastNumric = false
+          lastDot = false
+      }
+    }
+
+    private fun isOperatorAdded(value: String) : Boolean {
+        return if (value.startsWith("-"))
+            false
+        else {
+            value.contains("/") || value.contains("*") || value.contains("+")
+                    || value.contains("-")
+        }
+    }
+}
